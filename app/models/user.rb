5d8430b7c66a1ac13675d9dca52c5eed02d38009
Chris Bankston <chris@chrisbankston.com>
2013-02-13 17:02:58 -0600

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :session_authenticatable, :rememberable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+
+  def to_s
+    username
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointGamingForums::Application.config.secret_token = '673165195c31839094f8638d228ffedc5e2d74dae78cb9d03f587fd43a64ab2fa1f7d8c0adc273e084f29a450dfd7fceae2aba071a7ab077526a2164ecb80ea5'
