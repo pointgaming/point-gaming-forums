aafb657124afdb27eb56d32567cbbb6657244895
Chris Bankston <chris@chrisbankston.com>
2013-02-21 20:40:37 -0600

    Updated app to track user points

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221191948) do
+ActiveRecord::Schema.define(:version => 20130222023005) do
 
   create_table "forem_categories", :force => true do |t|
     t.string   "name",       :null => false
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(:version => 20130221191948) do
     t.string   "forem_state",            :default => "pending_review"
     t.boolean  "forem_auto_subscribe",   :default => false
     t.string   "username"
+    t.integer  "points"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
