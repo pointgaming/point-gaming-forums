d79a0024df9b2faea8f2614041b7b06d1b2b46e7
Chris Bankston <chris@chrisbankston.com>
2013-03-06 14:00:58 -0600

    Added links to the drop down menu in the navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
 
+  def pg_user_id
+    session.has_key?('warden.user.user.key') ? session['warden.user.user.key'].at(1).first : nil
+  end
+  helper_method :pg_user_id
+
   def forem_user
     current_user
   end
