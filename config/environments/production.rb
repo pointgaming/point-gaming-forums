de36d47a40cfb0bb2a0286f1fd8009d11c681d75
Chris Bankston <chris@chrisbankston.com>
2013-07-04 22:08:26 -0500

    Added pg_start to precompile path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ PointGamingForums::Application.configure do
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( pg_start.js )
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   config.action_mailer.default_url_options = { :host => 'forums.pointgaming.com' }
